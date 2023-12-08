e1ccc4bcbbeb23b17d4ba946e8c3d1265b1a06f7
Yury Andrabaila <yandrabaila@gmail.com>
2023-12-08 13:48:00 +0100

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 
 import './App.css'
+import { BrowserRouter } from 'react-router-dom';
+import { Routing } from './app/routing/Routing';
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+    <Routing />
+</BrowserRouter>
   )
 }
 
