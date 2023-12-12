31fc977d242ece683a82d3d1563eccc75e9d7e24
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-12-12 11:48:39 +0100

    feat: add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import './App.css'
+import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Routing } from './app/routing/Routing';
 import { StrictMode } from 'react';
 import { AppProvider } from './app/context';
 
 function App() {
-  return (
-    <StrictMode>
-      <AppProvider>
-        <BrowserRouter>
-          <Routing />
-        </BrowserRouter>
-      </AppProvider>
-    </StrictMode>
-  );
+    return (
+        <StrictMode>
+            <AppProvider>
+                <BrowserRouter>
+                    <Routing />
+                </BrowserRouter>
+            </AppProvider>
+        </StrictMode>
+    );
 }
 
 export default App;
