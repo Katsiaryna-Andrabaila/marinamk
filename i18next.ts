58c955b59093ce3028b571853d6e4a252df5394d
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-12-12 12:02:08 +0100

    feat: connect i18next to app

--- i18next.ts
+++ i18next.ts
@@ -1,25 +1,25 @@
 import en from './src/shared/assets/langs/en.json';
 import ru from './src/shared/assets/langs/ru.json';
 import ar from './src/shared/assets/langs/ar.json';
-import i18next from "i18next";
-import { initReactI18next } from "react-i18next";
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
 
 i18next.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: en,
+    resources: {
+        en: {
+            translation: en,
+        },
+        ru: {
+            translation: ru,
+        },
+        ar: {
+            translation: ar,
+        },
     },
-    ru: {
-      translation: ru,
-    },
-    ar: {
-        translation: ar,
-    },
-  },
-  lng: 'en',
-  fallbackLng: 'ru',
+    lng: 'en',
+    fallbackLng: 'ru',
 
-  interpolation: {
-    escapeValue: false,
-  },
+    interpolation: {
+        escapeValue: false,
+    },
 });
--- src/entities/langs/index.tsx
+++ src/entities/langs/index.tsx
@@ -1,9 +1,18 @@
-export const Langs = ()=> {
+import { useContext } from 'react';
+import { AppContext } from '../../app/context';
+
+export const Langs = () => {
+    const { setLang } = useContext(AppContext);
+
+    const handleChangeLang = (language: string) => {
+        setLang && setLang(language);
+    };
+
     return (
-    <div>
-        <span>EN</span>
-        <span>RU</span>
-        <span>AR</span>
-    </div>
-    )
-}
+        <div>
+            <span onClick={() => handleChangeLang('en')}>EN</span>
+            <span onClick={() => handleChangeLang('ru')}>RU</span>
+            <span onClick={() => handleChangeLang('ar')}>AR</span>
+        </div>
+    );
+};
