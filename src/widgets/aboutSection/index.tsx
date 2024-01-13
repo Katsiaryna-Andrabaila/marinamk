f1125628dae66250fc6ebbd23090f3f2d5cbcff5
Katsiaryna Andrabaila <androbailoe@gmail.com>
2024-01-14 00:05:40 +0100

    feat: add images to about section

--- src/widgets/aboutSection/index.tsx
+++ src/widgets/aboutSection/index.tsx
@@ -8,6 +8,16 @@ export const AboutSection = () => {
         <section className="about-section" id="about">
             <h2>{t('aboutHeader')}</h2>
             <p>{t('aboutContent')}</p>
+            <div className="about_images">
+                {new Array(6).fill(1).map((el, i) => (
+                    <img
+                        src={`src/shared/assets/img/about_nails${el + i}.png`}
+                        key={i}
+                        alt="Nail service"
+                        className={`about_image${el + i}`}
+                    />
+                ))}
+            </div>
         </section>
     );
 };
