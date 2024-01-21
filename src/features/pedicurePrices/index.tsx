9d787bfaf5d35b2ff41d83c390d986058a20ce5f
Katsiaryna Andrabaila <androbailoe@gmail.com>
2024-01-21 22:58:02 +0100

    feat: add pedicure prices

--- src/features/pedicurePrices/index.tsx
+++ src/features/pedicurePrices/index.tsx
@@ -1,3 +1,25 @@
+import { useTranslation } from 'react-i18next';
+
 export const PedicurePrices = () => {
-    return <></>;
+    const { t } = useTranslation();
+
+    return (
+        <ul className="service_prices">
+            <li className="service_prices_unit pedicure_basic">
+                <h3>{t('price.pedicure')}</h3>
+                <div className="service_price">
+                    <span>{t('price.pedicure_without')}</span>
+                    <span>$ 20</span>
+                </div>
+                <div className="service_price">
+                    <span>{t('price.pedicure_coating')}</span>
+                    <span>$ 35</span>
+                </div>
+                <div className="service_price">
+                    <span>{t('price.pedicure_removal_coating')}</span>
+                    <span>$ 40</span>
+                </div>
+            </li>
+        </ul>
+    );
 };
