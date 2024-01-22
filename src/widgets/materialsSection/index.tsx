459cf475092a8ebb37c0edacf0fcf3abb528a6d7
Katsiaryna Andrabaila <androbailoe@gmail.com>
2024-01-22 23:50:56 +0100

    feat: add materials section

--- src/widgets/materialsSection/index.tsx
+++ src/widgets/materialsSection/index.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import './materialsSection.styles.scss';
 import { useTranslation } from 'react-i18next';
 
@@ -7,6 +8,33 @@ export const MaterialsSection = () => {
     return (
         <section className="materials-section" id="materials">
             <h2>{t('materialsHeader')}</h2>
+            <p>{t('materialsContent')}</p>
+            <div className="materials_images">
+                <NavLink to="https://voguenailsru.ru/" target="_blank">
+                    <img
+                        src="https://voguenailsru.ru/upload/medialibrary/a73/a73572cc88d26b16971cf038879d41ea.png"
+                        className="vogue_img"
+                    />
+                </NavLink>
+                <NavLink to="https://n-club.pro/" target="_blank">
+                    <img src="https://n-club.pro/img/nail-club-logo-1642600196.jpg" />
+                </NavLink>
+                <NavLink to="https://runail.ru/" target="_blank">
+                    <img src="https://runail.ru/local/styles/img/logo.webp" />
+                </NavLink>
+                <NavLink to="https://holymollynails.eu/" target="_blank">
+                    <img src="https://holymollynails.eu/wp-content/uploads/2021/09/cropped-holymollylogo-4-145x60.png" />
+                </NavLink>
+                <NavLink to="https://fresh-prof.ru/" target="_blank">
+                    <img src="https://fresh-prof.ru/upload/CNext/a39/a391537b85e441df2ae14a8bb8178a91.jpg" />
+                </NavLink>
+                <NavLink to="https://miis.pro/" target="_blank">
+                    <img src="https://miis.pro/storage/img/logo-dark.png" />
+                </NavLink>
+                <NavLink to="https://opzia.ru/" target="_blank">
+                    <img src="https://opzia.ru/wp-content/themes/opzia-adm/image/svg/logo.svg" />
+                </NavLink>
+            </div>
         </section>
     );
 };
