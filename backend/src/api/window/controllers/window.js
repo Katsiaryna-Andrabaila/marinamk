677b589dcf876ce47f4265ca9411c62eae22ae58
Katsiaryna Andrabaila <androbailoe@gmail.com>
2024-02-04 20:16:40 +0100

    feat: add back and swr

---
+++ backend/src/api/window/controllers/window.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * window controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::window.window');
