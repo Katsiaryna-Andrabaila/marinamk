64d62698bf8ace7c3577a2b8f53d790b5df42e5f
Katsiaryna Andrabaila <androbailoe@gmail.com>
2024-02-09 21:43:00 +0100

    feat: include only available time slots from api in modal

--- src/features/timeInputs/index.tsx
+++ src/features/timeInputs/index.tsx
@@ -1,5 +1,6 @@
 import { Post } from '@prisma/client';
 import { AppointmentFormType } from 'entities/modal/lib/types';
+import { useEffect, useState } from 'react';
 import { FieldErrors, UseFormRegister, UseFormWatch } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
@@ -19,29 +20,43 @@ export const TimeInputs = ({
     const { t } = useTranslation();
     const date = watch('date');
     const radio = watch('time');
-    const time = ['9:00', '11:00', '13:00', '15:00', '17:00', '19:00'];
+    const [time, setTime] = useState<string[]>([]);
 
     const checkedStyle = {
         color: 'white',
         backgroundColor: '#216ba5',
     };
 
+    useEffect(() => {
+        if (date) {
+            const targetTime = JSON.parse(JSON.stringify(slots))
+                .filter(
+                    (el: Post) => new Date(el.date).getTime() === date.getTime()
+                )
+                .map((el: Post) => el.time);
+
+            setTime(targetTime);
+        }
+    }, [date, slots]);
+
     return (
         <div className="time_wrapper">
-            {time.map((el) => (
-                <div className="time_item" key={el}>
-                    <label style={radio === el ? checkedStyle : {}}>
-                        <input
-                            type="radio"
-                            value={el}
-                            {...register('time', {
-                                required: true,
-                            })}
-                        />
-                        {el}
-                    </label>
-                </div>
-            ))}
+            {time.length
+                ? time.map((el: string) => (
+                      <div className="time_item" key={el}>
+                          <label style={radio === el ? checkedStyle : {}}>
+                              <input
+                                  type="radio"
+                                  value={el}
+                                  {...register('time', {
+                                      required: true,
+                                  })}
+                              />
+                              {el}
+                          </label>
+                      </div>
+                  ))
+                : null}
             {errors?.time && (
                 <p style={{ color: '#FF3F25', fontSize: '13px' }}>
                     {t('selectTimeError')}
