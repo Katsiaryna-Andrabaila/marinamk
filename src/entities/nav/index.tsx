e7d2b944b4be3b0d882079d1ff542decbdceb84a
Katsiaryna Andrabaila <androbailoe@gmail.com>
2024-02-17 23:28:07 +0100

    feat: add burger

--- src/entities/nav/index.tsx
+++ src/entities/nav/index.tsx
@@ -1,34 +1,50 @@
 import './nav.styles.scss';
 import { Link } from 'react-scroll';
 import { useTranslation } from 'react-i18next';
+import { Dispatch, SetStateAction } from 'react';
+import useMatchMedia from '@buildinams/use-match-media';
 
-export const Nav = () => {
+export const Nav = ({
+    setIsOpen,
+}: {
+    setIsOpen?: Dispatch<SetStateAction<boolean>>;
+}) => {
     const { t } = useTranslation();
+    const isDesktop = useMatchMedia('(min-width: 1024px)');
+
+    const handleClick = () => {
+        !isDesktop && setIsOpen !== undefined && setIsOpen(false);
+    };
 
     return (
-        <nav>
+        <nav onClick={(event) => event.stopPropagation()}>
             <li>
-                <Link to="about" activeClass="active">
+                <Link to="about" activeClass="active" onClick={handleClick}>
                     {t('nav.about')}
                 </Link>
             </li>
             <li>
-                <Link to="prices" activeClass="active">
+                <Link to="prices" activeClass="active" onClick={handleClick}>
                     {t('nav.prices')}
                 </Link>
             </li>
             <li>
-                <Link to="materials" activeClass="active">
+                <Link to="materials" activeClass="active" onClick={handleClick}>
                     {t('nav.materials')}
                 </Link>
             </li>
             <li>
-                <Link to="feedback" activeClass="active">
+                <Link to="feedback" activeClass="active" onClick={handleClick}>
                     {t('nav.feedback')}
                 </Link>
             </li>
             <li>
-                <Link to="footer" activeClass="active" spy>
+                <Link
+                    to="footer"
+                    activeClass="active"
+                    spy
+                    onClick={handleClick}
+                >
                     {t('nav.contact')}
                 </Link>
             </li>
