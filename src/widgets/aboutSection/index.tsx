b4fc51fce8a72d6d3d0a465854317b71014d4aea
Katsiaryna Andrabaila <androbailoe@gmail.com>
2024-02-18 19:48:17 +0100

    feat: add adaptive to about section

--- src/widgets/aboutSection/index.tsx
+++ src/widgets/aboutSection/index.tsx
@@ -7,19 +7,21 @@ export const AboutSection = () => {
 
     return (
         <section className="about-section" id="about">
-            <h2>{t('aboutHeader')}</h2>
-            <p>{t('aboutContent')}</p>
-            <div className="about_images">
-                {new Array(6).fill(1).map((el, i) => (
-                    <Image
-                        src={`/img/about_nails${el + i}.png`}
-                        key={i}
-                        alt="Nail service"
-                        className={`about_image${el + i}`}
-                        fill={true}
-                        sizes="(max-width: 768px) 100%, (max-width: 1200px) 50%"
-                    />
-                ))}
+            <div className="about-wrapper">
+                <h2>{t('aboutHeader')}</h2>
+                <p>{t('aboutContent')}</p>
+                <div className="about_images">
+                    {new Array(6).fill(1).map((el, i) => (
+                        <Image
+                            src={`/img/about_nails${el + i}.png`}
+                            key={i}
+                            alt="Nail service"
+                            className={`about_image${el + i}`}
+                            fill={true}
+                            sizes="(max-width: 768px) 100%, (max-width: 1200px) 50%"
+                        />
+                    ))}
+                </div>
             </div>
         </section>
     );
