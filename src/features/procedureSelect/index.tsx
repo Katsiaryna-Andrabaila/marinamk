24a33f6ef4400c72dfb1abe30eca870c1a1c9d05
Katsiaryna Andrabaila <androbailoe@gmail.com>
2024-02-24 18:38:31 +0100

    feat: rewrite select in modal

--- src/features/procedureSelect/index.tsx
+++ src/features/procedureSelect/index.tsx
@@ -1,7 +1,8 @@
+import { useEffect, useState } from 'react';
 import { AppointmentFormType } from '../../entities/modal/lib/types';
 import { FieldErrors, UseFormRegister } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
-import { procedures } from 'shared/const/procedures';
+import { Service } from '@prisma/client';
 
 type ProcedureSelectProps = {
     register: UseFormRegister<AppointmentFormType>;
@@ -10,31 +11,47 @@ type ProcedureSelectProps = {
 
 export const ProcedureSelect = ({ register, errors }: ProcedureSelectProps) => {
     const { t } = useTranslation();
+    const [data, setData] = useState<Service[] | null>(null);
+
+    useEffect(() => {
+        fetch('/api/service')
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data);
+            });
+    }, []);
 
     return (
         <div className="select_wrapper">
             <p className="select_header">{t('selectProcedure')}</p>
-            <select
-                {...register('procedure', {
-                    required: true,
-                })}
-                className="procedure_select"
-            >
-                <optgroup label="manicure">
-                    {procedures.manicure.map((el) => (
-                        <option key={el} value={el}>
-                            {t(`price.${el}`)}
-                        </option>
-                    ))}
-                </optgroup>
-                <optgroup label="pedicure">
-                    {procedures.pedicure.map((el) => (
-                        <option key={el} value={el}>
-                            {t(`price.${el}`)}
-                        </option>
-                    ))}
-                </optgroup>
-            </select>
+            {data && (
+                <select
+                    {...register('procedure', {
+                        required: true,
+                    })}
+                    className="procedure_select"
+                >
+                    <optgroup label={t('price.manicure')}>
+                        {data
+                            .filter((el) => el.category === 'manicure')
+                            .filter((el) => el.subcategory !== 'design')
+                            .map((el) => (
+                                <option key={el.id} value={el.name}>
+                                    {t(`price.${el.name}`) || el.name}
+                                </option>
+                            ))}
+                    </optgroup>
+                    <optgroup label={t('price.pedicure')}>
+                        {data
+                            .filter((el) => el.category === 'pedicure')
+                            .map((el) => (
+                                <option key={el.id} value={el.name}>
+                                    {t(`price.${el.name}`) || el.name}
+                                </option>
+                            ))}
+                    </optgroup>
+                </select>
+            )}
             {errors?.procedure && (
                 <p style={{ color: '#FF3F25', fontSize: '13px' }}>
                     {t('selectProcedureError')}
