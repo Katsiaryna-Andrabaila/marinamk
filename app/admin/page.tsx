5f32b9b7ae6f7ec66251421c752b94d1cefe9c1c
Katsiaryna Andrabaila <androbailoe@gmail.com>
2024-02-26 19:09:19 +0100

    feat: rewrite logout

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,20 +2,19 @@
 
 import { AppContext, AppProvider } from 'app/context';
 import { AdminContent } from 'widgets/adminContent';
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
 import { useContext, useEffect, useState } from 'react';
 import { IS_ADMIN } from 'shared/const/isAdmin';
 import { Spinner } from 'features/spinner';
 
 function AdminPage() {
     const { isAdmin } = useContext(AppContext);
-    const router = useRouter();
     const [isLoading, setIsLoading] = useState(true);
     console.log(isAdmin);
 
     useEffect(() => {
         if (!IS_ADMIN.isAdmin) {
-            router.replace('/enter');
+            redirect('/enter');
         }
         setIsLoading(false);
     }, []);
