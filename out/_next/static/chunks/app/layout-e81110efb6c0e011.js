6d713899e7501a647d3c42ea8967d51d1cc967f1
Katsiaryna Andrabaila <androbailoe@gmail.com>
2024-03-06 11:34:26 +0100

    fix: fix db & hide /out

--- out/_next/static/chunks/app/layout-e81110efb6c0e011.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{3236:function(n,e,u){Promise.resolve().then(u.t.bind(u,277,23))},277:function(){}},function(n){n.O(0,[971,69,744],function(){return n(n.s=3236)}),_N_E=n.O()}]);
